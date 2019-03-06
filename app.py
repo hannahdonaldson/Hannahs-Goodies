bd1723599812e1804adbf4bfa13fb91fda69132f
hannahdonaldson <hannahcdonaldson@gmail.com>
2019-03-06 11:11:14 -0700

    attached daatbase and created a post route

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tlbjnprzxwowva:db0bc8667316928abf4ac3f261690a8459d0f33b1128149b9511ad0fe4daaa66@ec2-54-163-234-88.compute-1.amazonaws.com:5432/d3b8cct3rntuim'
 
 heroku = Heroku(app)
 db = SQLAlchemy(app)
@@ -27,6 +27,18 @@ class Book(db.Model):
 @app.route('/')
 def home():
     return "<h1>Hi from Flask</h1>"
+
+@app.route('/book/input', methods = ['POST'])
+def books_input():
+    if request.content_type == 'application/json':
+        post_data = request.get_json()
+        title = post_data.get('title')
+        author = post_data.get('author')
+        reg = Book(title, author)
+        db.session.add(reg)
+        db.session.commit()
+        return jsonify("Data Posted")
+    return jsonify('Somethings amiss')
 
 if __name__ == '__main__':
     app.debug = True
