b7a8e1cbf461529323c4d36948183586795c314f
hannahdonaldson <hannahcdonaldson@gmail.com>
2019-03-28 17:41:04 -0600

    Test

--- app.py
+++ app.py
@@ -1,76 +1,3 @@
-# from flask import Flask, request, jsonify
-# from flask_sqlalchemy import SQLAlchemy 
-# from flask_cors import CORS 
-# from flask_heroku import Heroku 
-
-# app = Flask(__name__)
-# CORS(app)
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://tlbjnprzxwowva:db0bc8667316928abf4ac3f261690a8459d0f33b1128149b9511ad0fe4daaa66@ec2-54-163-234-88.compute-1.amazonaws.com:5432/d3b8cct3rntuim'
-
-# heroku = Heroku(app)
-# db = SQLAlchemy(app)
-
-# class Book(db.Model):
-#     __tablename__ ="books"
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(120))
-#     author = db.Column(db.String(80))
-
-#     def __init__(self, title, author):
-#         self.title = title
-#         self.author = author
-
-#     def __repr__(self):
-#         return '<Title %r>' % self.title
-#         # This is string interpalation in python
-#         # the % self.title is setting a value to %r
-
-
-# @app.route("/")
-# def home():
-#     return "<h1>Hello World</h1>"
-
-
-# @app.route('/book/input', methods=['POST'])
-# def books_input():
-#     if request.content_type == 'application/json':
-#         post_data = request.get_json()
-#         title = post_data.get('title')
-#         author = post_data.get('author')
-#         reg = Book(title, author)
-#         db.session.add(reg)
-#         db.session.commit()
-#         return jsonify("Data Posted")
-#     return jsonify("Something went wrong")
-
-
-# @app.route('/books', methods=['GET'])
-# def return_books():
-#     all_books = db.session.query(Book.id, Book.title, Book.author).all()
-#     return jsonify(all_books)
-
-# @app.route('/book/<id>', methods = ['GET'])
-# def return_single_book(id):
-#     one_book = db.session.query(Book.id, Book.title, Book.author).filter(Book.id == id).first()
-#     return jsonify(one_book)
-
-# @app.route('/delete/<id>', methods=['DELETE'])
-# def book_delete(id):
-#     if request.content_type == 'application/json':
-#         record = db.session.query(Book).get(id)
-#         db.session.delete(record)
-#         db.session.commit()
-#         return jsonify("Completed Delete Mehtod")
-#     return jsonify("Delete Failed")
-
-# if __name__ == '__main__':
-#     app.debug = True
-#     app.run()
-
-
-
-
-
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy 
 from flask_cors import CORS 
@@ -78,7 +5,7 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://dxonitmdctxush:1766abdc44fbd2422787720371381c54e0612771d3b7bef3e43201a3dd281856@ec2-174-129-10-235.compute-1.amazonaws.com:5432/d32n2gvkl5pork'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://bxsswwybqhvhic:b2fddf8a534912f868473b8fe9e66f66e153dbb02febfe1cb8b7041bfa11cb70@ec2-23-23-241-119.compute-1.amazonaws.com:5432/d4ibc8jtbqqp86'
 
 heroku = Heroku(app)
 db = SQLAlchemy(app)
@@ -159,3 +86,87 @@ if __name__ == '__main__':
     app.debug = True
     app.run()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask, request, jsonify
+# from flask_sqlalchemy import SQLAlchemy 
+# from flask_cors import CORS 
+# from flask_heroku import Heroku 
+
+# app = Flask(__name__)
+# CORS(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://tlbjnprzxwowva:db0bc8667316928abf4ac3f261690a8459d0f33b1128149b9511ad0fe4daaa66@ec2-54-163-234-88.compute-1.amazonaws.com:5432/d3b8cct3rntuim'
+
+# heroku = Heroku(app)
+# db = SQLAlchemy(app)
+
+# class Book(db.Model):
+#     __tablename__ ="books"
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(120))
+#     author = db.Column(db.String(80))
+
+#     def __init__(self, title, author):
+#         self.title = title
+#         self.author = author
+
+#     def __repr__(self):
+#         return '<Title %r>' % self.title
+#         # This is string interpalation in python
+#         # the % self.title is setting a value to %r
+
+
+# @app.route("/")
+# def home():
+#     return "<h1>Hello World</h1>"
+
+
+# @app.route('/book/input', methods=['POST'])
+# def books_input():
+#     if request.content_type == 'application/json':
+#         post_data = request.get_json()
+#         title = post_data.get('title')
+#         author = post_data.get('author')
+#         reg = Book(title, author)
+#         db.session.add(reg)
+#         db.session.commit()
+#         return jsonify("Data Posted")
+#     return jsonify("Something went wrong")
+
+
+# @app.route('/books', methods=['GET'])
+# def return_books():
+#     all_books = db.session.query(Book.id, Book.title, Book.author).all()
+#     return jsonify(all_books)
+
+# @app.route('/book/<id>', methods = ['GET'])
+# def return_single_book(id):
+#     one_book = db.session.query(Book.id, Book.title, Book.author).filter(Book.id == id).first()
+#     return jsonify(one_book)
+
+# @app.route('/delete/<id>', methods=['DELETE'])
+# def book_delete(id):
+#     if request.content_type == 'application/json':
+#         record = db.session.query(Book).get(id)
+#         db.session.delete(record)
+#         db.session.commit()
+#         return jsonify("Completed Delete Mehtod")
+#     return jsonify("Delete Failed")
+
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run()
