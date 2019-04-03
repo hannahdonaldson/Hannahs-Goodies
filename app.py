36f9de956f2d95da9efaf50010ad3892cf50a44c
hannahdonaldson <hannahcdonaldson@gmail.com>
2019-04-03 16:11:50 -0600

    added config file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template,
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy 
 from flask_cors import CORS 
 from flask_heroku import Heroku 
